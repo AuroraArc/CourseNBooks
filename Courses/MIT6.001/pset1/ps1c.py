b0b8e2064b26fbdc4d4e9cc7305869022d467ad3
AuroraArc <33708423+AuroraArc@users.noreply.github.com>
2023-05-07 01:14:45 -0500

    added semi-annual raise component

--- Courses/MIT6.001/pset1/ps1c.py
+++ Courses/MIT6.001/pset1/ps1c.py
@@ -7,21 +7,32 @@ down_payment = total_cost * portion_down_payment
 current_savings = 0
 r = 0.04
 months = 36
+total_salary = 0
+i = 1
+
+for i in range(months + 1):
+    if months % 6 == 0:
+        annual_salary = annual_salary + annual_salary * semi_annual_raise
+    total_salary += annual_salary
+    months += 1
+annual_salary = total_salary / 36.0
+monthly_salary = annual_salary / 12.0
 
 count = 0
 epsilon = 100
 low = 0
-high = round(down_payment / (36.0 * annual_salary / 12.0), 4) # looks for portion saved variable
-guess_savings = round((low + high) / 2.0, 4)
+high = round(down_payment / (36.0 * monthly_salary), 4) # looks for portion saved variable
+percent_savings = round((low + high) / 2.0, 4)
+factor = 1.04 ** 3
 
 
-while abs(annual_salary / 12.0 * guess_savings * 36.0 - down_payment) > epsilon:
-    debug = abs(annual_salary / 12.0 * guess_savings * 36.0 - down_payment)
-    if guess_savings * 36.0 < down_payment:
-        low = guess_savings
+while abs((monthly_salary * percent_savings * 36.0)- down_payment) > epsilon:
+    debug = abs((monthly_salary * percent_savings * 36.0)- down_payment)
+    if (monthly_salary * percent_savings * 36.0) < down_payment:
+        low = percent_savings
     else:
-        high = guess_savings
-    guess_savings = round((low + high) / 2.0, 4)
+        high = percent_savings
+    percent_savings = round((low + high) / 2.0, 4)
     count += 1
-print(f"Best savings rate: {guess_savings}")
+print(f"Best savings rate: {percent_savings}")
 print(f"Steps in bisections search: {count}")
