f41933e1a84b8d2f2389cc97ff7ae1df02645dd5
AuroraArc <33708423+AuroraArc@users.noreply.github.com>
2023-05-18 15:56:48 -0500

    Update CS61B.java

--- Courses/CS61B/!disc/2.2/CS61B.java
+++ Courses/CS61B/!disc/2.2/CS61B.java
@@ -1,21 +1,41 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CS61B {
     // variables (part b)
-    private final String university = "UC Berkeley";
+    private static String university = "UC Berkeley";
     private String semester;
-    private List<String> students;
-    private HashMap<Integer, String> idToStudent;
+    private CS61BStudent[] students;
+    private Map<Integer, CS61BStudent> idToStudent = new HashMap<>();
 
     // constructor (parts a, c)
-    public CS61B(int capacity, HashMap<Integer, String> ids, String semester) {
-        students = (List<String>)ids.values();
-        idToStudent = ids;
+    public CS61B(int capacity, List<Integer> ids, String semester) {
+        students = new CS61BStudent[capacity];
+        for (int i = 0; i < capacity; i++) {
+            Integer newStudentId = ids.get(i);
+            CS61BStudent newStudent = new CS61BStudent(newStudentId);
+            students[i] = newStudent;
+            idToStudent.put(newStudentId, newStudent);
+        }
         this.semester = semester;
     }
 
     //methods (part d)
+    public void makeAllStudentsWatchLecture() {
+        for (CS61BStudent student : students) {
+            student.watchLecture();
+        }
+    }
+
+    public int updateGrade(Integer sid, int points) {
+        CS61BStudent student = idToStudent.get(sid);
+        student.grade += points;
+        return student.grade;
+    }
+
+    public static void changeUniversity(String newUniversity) {
+        university = newUniversity;
+    }
 
 }
